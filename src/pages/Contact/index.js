b37d4abdb6e4d31c6eaf9c13485da10aa8d6cf40
Tala Ammoun <talaammoun@gmail.com>
2023-03-17 20:36:42 +0000

    integrated Tailwind for styling, yup verification system, Formik for forms, and email.js to send messages to my email

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,22 +1,20 @@
-import React, { Component }from "react";
+import React from "react";
 import { Route, Routes } from "react-router-dom";
-//import * as yup from 'yup';
-//import emailjs from '@emailjs/browser';
-import styles from "./styles.module.css";
-import Form from "./Form.js"
+import * as yup from 'yup';
+import Form from "./Form.js";
+
+export const userSchema = yup.object().shape({
+  fullName: yup.string().required("Name cannot be left blank."),
+  email: yup.string().email("Please enter a valid email.").required("Please enter a valid email."),
+  message:yup.string().min(10).max(500).required("This field is required."),
+});
 
 export default function Contact() {
-    // let userSchema = yup.object().shape({
-    //     fullName: yup.string().required("Name cannot be left blank."),
-    //     email: yup.string().email().required("Please enter a valid email."),
-    //     message:yup.string().max(500).required("This field is required."),
-    //   });
   return (
-    <div className={styles.card}>
-      <h1 className={styles.heading}>Contact Us</h1>
+    <div className="m-2 p-10 bg-slate-200">
+      <h1 className="text-center font-serif bg-red-900 min-h-100
+      leading-3 text-2xl  text-white py-10 px-20">Contact Us</h1>
       <Form></Form>
     </div>
   );
 }
-
-
